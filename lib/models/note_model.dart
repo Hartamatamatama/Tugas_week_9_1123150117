f4c39a77d8a5391991217161a0a15b0fe81e61c4
hartamatamatama <katonojutsu340@gmail.com>
2025-11-29 18:32:47 +0700

    Membuat note_model.dart

---
+++ lib/models/note_model.dart
@@ -0,0 +1,23 @@
+class Note {
+  final int? id;
+  final String title;
+  final String content;
+  Note({this.id, required this.title, required this.content});
+  factory Note.fromMap(Map<String, dynamic> json) =>
+      Note(content: json['content'], title: json['title'], id: json['id']);
+  // factory Note.fromMap(Map<String, dynamic> json) {
+  // return Note(
+  // content: json['content'],
+  // title: json['title'],
+  // id : json['id'],
+  // );
+  // }
+  // factory Note.fromMap(Map<String, dynamic> json) => Note(
+  // content: json['content'] ?? '',
+  // title: json['title'] ?? '',
+  // id : json['id'] ? 0,
+  // );
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'title': title, 'content': content};
+  }
+}
